7c1eefda3f9e286e47d9a04bd9c5c1fe989feced
githubstuden <784884371@qq.com>
2019-03-10 14:56:10 +0800

    登录页验证

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,13 +1,89 @@
 <template>
-    <div>
-        登录
-    </div>
+  <div class="login">
+    <el-form
+      :model="formData"
+      status-icon
+      ref="ruleForm2"
+      label-width="100px"
+      class="demo-ruleForm"
+      label-position="top"
+      :rules="rules"
+    >
+      <h2>用户登录</h2>
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model.trim="formData.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" v-model.trim="formData.password" autocomplete="off"></el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button class="form-btn" type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      formData: {
+        username: "",
+        password: ""
+      },
+      rules: {
+    username: [
+      { required: true, message: "用户名不能为空", trigger: "blur" },
+      { min: 3, max: 8, message: "长度在 3 到 8 个字符", trigger: "change" }
+    ],
+    password: [
+      { required: true, message: "密码不能为空", trigger: "blur" },
+      { min: 3, max: 20, message: "长度在 3 到 20 个字符", trigger: "change" }
+    ]
+  }
+    };
+  },
+  methods:{
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            this.$message.error('请输入正确的用户名或密码');
+            return false;
+          }
+        });
+      },
+  }
+  
+};
 </script>
 <style>
-
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+body > div:first-of-type {
+  height: 100%;
+}
+.login {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #324152;
+}
+.el-form {
+  width: 580px;
+  height: 440px;
+  background-color: #fff;
+  box-sizing: border-box;
+  padding: 40px;
+  border-radius: 10px;
+}
+.form-btn {
+  width: 100%;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from "./lib/router"
 Vue.config.productionTip = false
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 new Vue({
   router,
   render: h => h(App),
